b634087dade5f2b4ad40f86b9343cb7828b59444
Praveen Bishnoi <praveenpraveenbishnoi07@gmail.com>
2026-07-07 12:50:41 +0530

    Added Contact section and Footer

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Praveen AI Portfolio Loaded Successfully 🚀");
+const form = document.querySelector(".contact-form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+});

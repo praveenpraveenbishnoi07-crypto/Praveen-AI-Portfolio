c5d8c12c5906d79b46f2fe4e1f2497a7ed8e9639
Praveen Bishnoi <praveenpraveenbishnoi07@gmail.com>
2026-07-07 12:58:22 +0530

    Final website polish and animations

--- script.js
+++ script.js
@@ -35,3 +35,19 @@ backBtn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.2
+});
+
+sections.forEach(section=>{
+    section.classList.add("fade-up");
+    observer.observe(section);
+});

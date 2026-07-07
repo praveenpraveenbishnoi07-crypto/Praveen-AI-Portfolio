08418bf363d7ce6791e081f16fbb5216d0c0a045
Praveen Bishnoi <praveenpraveenbishnoi07@gmail.com>
2026-07-07 12:57:33 +0530

    Added dark mode and back to top button

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 console.log("Stats section loaded successfully.");
+const themeBtn = document.getElementById("theme-toggle");
+const backBtn = document.getElementById("backToTop");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+    themeBtn.textContent =
+        document.body.classList.contains("light-theme") ? "☀️" : "🌙";
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backBtn.classList.add("show");
+    } else {
+        backBtn.classList.remove("show");
+    }
+});
+
+backBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});

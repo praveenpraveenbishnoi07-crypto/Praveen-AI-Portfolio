c23be18712f5d2f574856fd33c2513d046b51cab
Praveen Bishnoi <praveenpraveenbishnoi07@gmail.com>
2026-07-07 12:52:49 +0530

    Added responsive mobile navigation

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ form.addEventListener("submit", function(e){
     e.preventDefault();
     alert("Thank you! Your message has been sent.");
 });
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
